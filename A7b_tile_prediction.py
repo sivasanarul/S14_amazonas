e2ec8272c5468f320571f84e2e5298bc8f790525
EO User <eouser@internal-development.novalocal>
2024-03-05 06:26:10 +0000

    consistent tile prediction

--- A7b_tile_prediction.py
+++ A7b_tile_prediction.py
@@ -44,7 +44,7 @@ moving_window_size = block_size - (inner_buffer*2)
 
 for tile_item in tiles:
 
-    predicted_baselc_tile_prob_path = predicted_baselc_folder.joinpath(f"{tile_item}_BASELC_{year}_PROB_edges_witoverlap.tif")
+    predicted_baselc_tile_prob_path = predicted_baselc_folder.joinpath(f"{tile_item}_BASELC_{year}_PROB.tif")
     predicted_baselc_tile_class_path = predicted_baselc_folder.joinpath(f"{tile_item}_BASELC_{year}_CLASS.tif")
 
     tile_prediction_year_mosaic_tif = merged_sar_ref_folder.joinpath(f"{tile_item}_BAC_MERGED_{year}.tif")
@@ -60,11 +60,6 @@ for tile_item in tiles:
      imagery_extent_box) = read_raster_info(str(tile_prediction_year_mosaic_tif))
 
 
-
-
-    window_size = block_size
-
-
     def adjust_window_position(size, start, window_size):
         # Adjust window position if it goes beyond the image size
         if start + window_size > size:
@@ -72,43 +67,73 @@ for tile_item in tiles:
         return start
 
 
+    window_size = block_size
+    # first lets do the four corners
+    window = dataset.ReadAsArray(0, 0, window_size, window_size)
+    processed_window = process_window(window, loaded_model)
+    prediction_prob[0:window_size, 0: window_size] = processed_window
+
+    window = dataset.ReadAsArray((cols - window_size), 0, window_size, window_size)
+    processed_window = process_window(window, loaded_model)
+    prediction_prob[0:window_size, (cols - window_size): cols] = processed_window
+
+    window = dataset.ReadAsArray(0, (rows - window_size), window_size, window_size)
+    processed_window = process_window(window, loaded_model)
+    prediction_prob[(rows - window_size): rows, 0: window_size] = processed_window
+
+    window = dataset.ReadAsArray((cols - window_size), (rows - window_size), window_size, window_size)
+    processed_window = process_window(window, loaded_model)
+    prediction_prob[(rows - window_size): rows, (cols - window_size): cols] = processed_window
+
+    inner_buffer_window = window_size - inner_buffer
     # Process edges
     for edge in ['top', 'bottom', 'left', 'right']:
         if edge in ['top', 'bottom']:
-            for start_col in range(0, cols, window_size):
-                start_col = adjust_window_position(cols, start_col, window_size)
+            for start_col in range(inner_buffer, cols, moving_window_size):
+
+                left_limit = start_col - inner_buffer
+                right_limit = left_limit + window_size
+                if right_limit > cols: continue
+
                 if edge == 'top':
-                    window = dataset.ReadAsArray(start_col, 0, window_size, window_size)
-                else:  # bottom
-                    window = dataset.ReadAsArray(start_col, rows - window_size, window_size, window_size)
-                processed_window = process_window(window, loaded_model)
-                if edge == 'top':
-                    prediction_prob[0:window_size, start_col:start_col + window_size] = processed_window
+                    window = dataset.ReadAsArray(left_limit, 0, window_size, window_size)
                 else:
-                    prediction_prob[rows - window_size:rows, start_col:start_col + window_size] = processed_window
+                    window = dataset.ReadAsArray(left_limit, rows - window_size, window_size, window_size)
+
+                processed_window = process_window(window, loaded_model)
+
+                if edge == 'top':
+                    prediction_prob[0:window_size, start_col:start_col + moving_window_size] = processed_window[:,
+                                                                                               inner_buffer:-inner_buffer]
+                else:
+                    prediction_prob[rows - window_size:rows,
+                    start_col:start_col + moving_window_size] = processed_window[:,
+                                                                inner_buffer:-inner_buffer]
+
         else:  # 'left' or 'right'
-            for start_row in range(0, rows, window_size):
-                start_row = adjust_window_position(rows, start_row, window_size)
+            for start_row in range(inner_buffer, rows, moving_window_size):
+
+                upper_limit = start_row - inner_buffer
+                lower_limit = upper_limit + window_size
+                if lower_limit > rows: continue
+
                 if edge == 'left':
-                    window = dataset.ReadAsArray(0, start_row, window_size, window_size)
+                    window = dataset.ReadAsArray(0, upper_limit, window_size, window_size)
                 else:  # right
-                    window = dataset.ReadAsArray(cols - window_size, start_row, window_size, window_size)
+                    window = dataset.ReadAsArray(cols - window_size, upper_limit, window_size, window_size)
+
                 processed_window = process_window(window, loaded_model)
+
                 if edge == 'left':
-                    prediction_prob[start_row:start_row + window_size, 0:window_size] = processed_window
+                    prediction_prob[start_row:start_row + moving_window_size, 0:window_size] = processed_window[
+                                                                                               inner_buffer:-inner_buffer,
+                                                                                               :]
                 else:
-                    prediction_prob[start_row:start_row + window_size, cols - window_size:cols] = processed_window
+                    prediction_prob[start_row:start_row + moving_window_size,
+                    cols - window_size:cols] = processed_window[inner_buffer:-inner_buffer, :]
 
-
-
-
-
-
-
-
-
-    for x_block in range(inner_buffer, RasterXSize+1, moving_window_size):
-        for y_block in range(inner_buffer, RasterYSize+1, moving_window_size):
+    for x_block in range(inner_buffer, RasterXSize + 1, moving_window_size):
+        for y_block in range(inner_buffer, RasterYSize + 1, moving_window_size):
 
             x_block_prediction_window = x_block - inner_buffer
             y_block_prediction_window = y_block - inner_buffer
@@ -125,16 +150,19 @@ for tile_item in tiles:
                 do_pred = False
 
             if do_pred:
-                chunk = dataset.ReadAsArray(x_block_prediction_window, y_block_prediction_window, block_size, block_size)
-    
+                chunk = dataset.ReadAsArray(x_block_prediction_window, y_block_prediction_window, block_size,
+                                            block_size)
+
                 transposed_chunk = chunk.transpose(1, 2, 0)
                 reshaped_data = np.expand_dims(transposed_chunk, axis=0)
                 result = loaded_model.predict(reshaped_data)
                 result = result[0]
-                inner_box = result[inner_buffer:-inner_buffer, inner_buffer:-inner_buffer, :]
-                inner_box_sq = np.squeeze(inner_box)
-    
-                prediction_prob[x_block: (x_block + moving_window_size), y_block: (y_block + moving_window_size)] = inner_box_sq
+                result_sq = np.squeeze(result)
+                inner_box = result_sq[inner_buffer:-inner_buffer, inner_buffer:-inner_buffer]
+
+                prediction_prob[y_block: (y_block + moving_window_size),
+                x_block: (x_block + moving_window_size)] = inner_box
+
 
     save_raster_template(str(tile_prediction_year_mosaic_tif), str(predicted_baselc_tile_prob_path), prediction_prob, GDT_Float32, nodata_value=None)
 
